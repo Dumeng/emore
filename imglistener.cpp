201623a793a20335ef338b68e1dba98ad7d1ccca
Dumeng <793763955@qq.com>
2017-02-09 23:33:55 +0800

    New UI

---
+++ imglistener.cpp
@@ -0,0 +1,6 @@
+#include "imglistener.h"
+
+imgListener::imgListener()
+{
+
+}
---
+++ imglistener.h
@@ -0,0 +1,11 @@
+#ifndef IMGLISTENER_H
+#define IMGLISTENER_H
+
+
+class imgListener
+{
+public:
+    imgListener();
+};
+
+#endif // IMGLISTENER_H
